14cf3552437a20635e31339b01f817f13fb8bee5
Alex Rodgers <alex.e.rodgers@gmail.com>
2017-01-01 15:28:09 -0800

    added chapter 1 file

---
+++ Chapter-1/Count.java
@@ -0,0 +1,23 @@
+//******
+// Count Program
+// Alex Rodgers
+// A program that counts to the number 5 in 3 different languages
+//******
+public class Count
+{
+
+    public static void main (String[] args)
+    {
+
+        //Counts to the number 5 in English    
+        System.out.println ("one two three four five");
+
+        //Counts to the number 5 in French
+        System.out.println ("un deux trois quatre cinq");
+
+        //Counts to the number 5 in Spanish
+        System.out.println ("uno dos tres cuatro cinco");
+
+    }
+
+}
---
+++ Chapter-1/Simple.java
@@ -0,0 +1,23 @@
+// ********************************************
+//  Simple.java
+// 
+//  Print a simple message about Java.
+//          
+// ********************************************
+
+public class Simple
+{
+
+    public static void main (String[] args)
+    {
+
+        System.out.println ("Java rocks!!");
+    }
+}
+//simple: It should start with an Uppercase character and match the name of the file
+//SimpleProgram: It is a legal identifier, a good choice for the program
+//1Simple: This is illegal, an identifier cannot start with a number.
+//_Simple_: This is allowed, the underscores may not be necessary however.
+//*Simple*: This identifier is illegal because it contains the * character.
+//$123_45: This identifier is legal because, but it doesn't give any information on what the variable holds
+//Simple!: This identifier is illegal because it contains the '!' character.
